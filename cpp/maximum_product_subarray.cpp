185dfb1902f7b1e90c93b6fadb68c66ee4f23be5
Solomon Wu <solomon.wzs@gmail.com>
2014-11-03 22:51:07 +0800

    new file:   cpp/maximum_product_subarray.cpp

---
+++ cpp/maximum_product_subarray.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  int maxProduct(int A[], int n){
+    int max=A[0];
+    int pmax=A[0];
+    int pmin=A[0];
+
+    for (int i=1; i<n; ++i){
+      int a=pmax*A[i];
+      int b=pmin*A[i];
+
+      if (a>b){
+        pmax=a;
+        pmin=b;
+      } else{
+        pmax=b;
+        pmin=a;
+      }
+
+      if (A[i]>pmax){
+        pmax=A[i];
+      }
+      if (A[i]<pmin){
+        pmin=A[i];
+      }
+
+      max=pmax>max?pmax:max;
+    }
+    return max;
+  }
+};
+
+int main(){
+  Solution s;
+  int arr[3]={3, -1, 4};
+
+  printf("%d\n", s.maxProduct(arr, 3));
+  return 0;
+}
