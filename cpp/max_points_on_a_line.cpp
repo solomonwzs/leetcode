360480a250d09375e8bdc020ffcc98b2fe8b6647
Solomon Wu <solomon.wzs@gmail.com>
2014-11-05 00:19:41 +0800

    new file:   cpp/max_points_on_a_line.cpp

---
+++ cpp/max_points_on_a_line.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+struct Point {
+  int x;
+  int y;
+  Point():x(0), y(0){}
+  Point(int a, int b):x(a), y(b){}
+};
+
+
+class Solution {
+ public:
+  int maxPoints(vector<Point> &points){
+  }
+};
+
+int main(){
+  return 0;
+}
