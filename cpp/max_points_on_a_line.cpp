983c1d7368765df5020ad329d70b3d686f8eb72e
Solomon Wu <solomon.wzs@gmail.com>
2014-11-05 23:08:10 +0800

    modified:   cpp/max_points_on_a_line.cpp

--- cpp/max_points_on_a_line.cpp
+++ cpp/max_points_on_a_line.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <stdio.h>
 #include <vector>
 
@@ -12,11 +13,63 @@ struct Point {
 
 
 class Solution {
+#define MAX 2147483647
+
  public:
   int maxPoints(vector<Point> &points){
+    if (points.size()==1){
+      return 1;
+    }
+
+    vector<float> slope;
+    int max_points=0;
+    
+    for (unsigned i=0; i<points.size(); ++i){
+      int same=0;
+      slope.clear();
+
+      for (unsigned j=0; j<points.size(); ++j){
+        if (i==j){
+          continue;
+        }
+
+        int delta_x=points[i].x-points[j].x;
+        int delta_y=points[i].y-points[j].y;
+        if (delta_x==0 && delta_y==0){
+          ++same;
+        } else if (delta_x==0){
+          slope.push_back(MAX);
+        } else{
+          slope.push_back((float)delta_y/delta_x);
+        }
+      }
+
+      float f=0;
+      int len=same+1;
+      sort(slope.begin(), slope.end());
+      for (vector<float>::iterator it=slope.begin(); it!=slope.end(); ++it){
+        if (f==*it){
+          ++len;
+        } else{
+          f=*it;
+          max_points=len>max_points?len:max_points;
+          len=same+2;
+        }
+      }
+      max_points=len>max_points?len:max_points;
+    }
+    return max_points;
   }
 };
 
 int main(){
+  Point p[9]={Point(84, 250), Point(0, 0), Point(1, 0), Point(0, -70), Point(0, -70), Point(1, -1), Point(21, 10), Point(42, 90), Point(-42, -230)};
+  vector<Point> v;
+  for (int i=0; i<9; ++i){
+    v.push_back(p[i]);
+  }
+
+  Solution s;
+  printf("%d\n", s.maxPoints(v));
   return 0;
 }
