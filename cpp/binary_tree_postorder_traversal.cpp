33753274e63b9b03fff5592e38a7e6391006c543
Solomon Ng <solomon.wzs@gmail.com>
2014-11-14 18:18:51 +0800

    new file:   cpp/binary_tree_postorder_traversal.cpp

---
+++ cpp/binary_tree_postorder_traversal.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+
+struct TreeNode{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x): val(x), left(NULL), right(NULL) {}
+};
+
+class Soluction{
+ public:
+  vector<int> postorderTraversal(TreeNode *root){
+    vector<int> v;
+    return v;
+  }
+};
+
+
+static void build_tree(TreeNode **p, int argc, char **argv, int *index){
+  if (*index<argc && strcmp(argv[*index], "#")==0){
+    *p=new TreeNode(atoi(argv[*index]));
+    ++(*index);
+
+    build_tree(&((*p)->left), argc, argv, index);
+    build_tree(&((*p)->right), argc, argv, index);
+  } else{
+    ++(*index);
+  }
+}
+
+
+static void clear_tree(TreeNode *p){
+  if (p->left){
+    clear_tree(p->left);
+  }
+  if (p->right){
+    clear_tree(p->right);
+  }
+  printf("%d\n", p->val);
+  delete p;
+}
+
+
+int main(int argc, char **argv){
+  TreeNode *root;
+
+  int index=1;
+  build_tree(&root, argc, argv, &index);
+  clear_tree(root);
+
+  return 0;
+}
