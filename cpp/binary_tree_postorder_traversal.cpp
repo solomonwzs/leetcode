fa394dcb0551ea63f55ba68b71801354fd7f2332
Solomon Wu <solomon.wzs@gmail.com>
2014-11-14 21:47:47 +0800

    modified:   cpp/binary_tree_postorder_traversal.cpp

--- cpp/binary_tree_postorder_traversal.cpp
+++ cpp/binary_tree_postorder_traversal.cpp
@@ -13,17 +13,30 @@ struct TreeNode{
   TreeNode(int x): val(x), left(NULL), right(NULL) {}
 };
 
-class Soluction{
+class Solution{
  public:
   vector<int> postorderTraversal(TreeNode *root){
     vector<int> v;
+    if (root){
+      this->postorderTraversal0(root, &v);
+    }
     return v;
+  }
+ private:
+  void postorderTraversal0(TreeNode *node, vector<int> *v){
+    if (node->left){
+      this->postorderTraversal0(node->left, v);
+    }
+    if (node->right){
+      this->postorderTraversal0(node->right, v);
+    }
+    v->push_back(node->val);
   }
 };
 
 
 static void build_tree(TreeNode **p, int argc, char **argv, int *index){
-  if (*index<argc && strcmp(argv[*index], "#")==0){
+  if (*index<argc && strcmp(argv[*index], "#")!=0){
     *p=new TreeNode(atoi(argv[*index]));
     ++(*index);
 
@@ -42,16 +55,22 @@ static void clear_tree(TreeNode *p){
   if (p->right){
     clear_tree(p->right);
   }
-  printf("%d\n", p->val);
+  //printf("%d\n", p->val);
   delete p;
 }
 
 
 int main(int argc, char **argv){
   TreeNode *root;
+  vector<int> v;
+  Solution s;
 
   int index=1;
   build_tree(&root, argc, argv, &index);
+  v=s.postorderTraversal(root);
+  for (vector<int>::iterator it=v.begin(); it!=v.end(); ++it){
+    printf("%d\n", *it);
+  }
   clear_tree(root);
 
   return 0;
