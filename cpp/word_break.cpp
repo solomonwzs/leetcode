3f87f65d7701bffc4ea52bde7c10bd8d7369bc84
Solomon Ng <solomon.wzs@gmail.com>
2014-11-20 11:20:26 +0800

    modified:   cpp/word_break.cpp 	modified:   project.vim

--- cpp/word_break.cpp
+++ cpp/word_break.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string>
 #include <unordered_set>
+#include <vector>
 
 using namespace std;
 
@@ -8,20 +9,46 @@ using namespace std;
 class Solution{
  public:
   bool wordBreak(string s, unordered_set<string> &dict){
-    if (s=="") return true;
-    for (unordered_set<string>::iterator it=dict.begin(); it!=dict.end(); ++it){
+    vector<bool> v;
+    for (unsigned i=0; i<=s.length(); ++i){
+      v.push_back(false);
     }
-    return true;
-  }
+    v[0]=true;
+
+    for (unsigned i=0; i<v.size(); ++i){
+      if (v[i]){
+        for (unordered_set<string>::iterator it=dict.begin();
+             it!=dict.end();
+             ++it){
+          unsigned len=it->length();
+          unsigned j=i+len;
+          if (j<v.size() && s.substr(i, len)==*it){
+            v[j]=true;
+          }
+        }
+      }
+    }
+
+    return v[v.size()-1];
   }
 };
 
 
 int main(){
-  string s="helloworld";
+  string str=
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+      "aaaaaab";
   unordered_set<string> dict;
-  dict.insert("hello");
-  dict.insert("world");
+  dict.insert("a");
+  dict.insert("aa");
+  dict.insert("aaa");
+  dict.insert("aaaa");
+  dict.insert("aaaaa");
+  dict.insert("aaaaaa");
+
+  Solution s;
+  printf("%d\n", s.wordBreak(str, dict));
 
   return 0;
 }
--- project.vim
+++ project.vim
@@ -1,2 +1,2 @@
-let g:syntastic_cpp_compiler_options='-std=c++11 -stdlib=libc++'
+let g:syntastic_cpp_compiler_options='-std=c++11'
 let g:clang_user_options="-std=c++11"
