b7e8d100e5d0122382088faaff176c693e0993fd
Solomon Ng <solomon.wzs@gmail.com>
2014-12-29 18:24:57 +0800

    new file:   cpp/palindrome_partitioning.cpp

---
+++ cpp/palindrome_partitioning.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include "utils.h"
+
+using namespace std;
+
+
+class Solution {
+ public:
+  vector< vector<string> > partition(string s) {
+  }
+};
+
+
+int main(int argc, char **argv){
+  string s=argv[1];
+
+  debug_log("%s\n", s.c_str());
+  return 0;
+}
