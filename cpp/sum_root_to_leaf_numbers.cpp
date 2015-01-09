fb31e47f0fac56d8b72954270d6a8b0c32c6912d
Solomon Ng <solomon.wzs@gmail.com>
2015-01-09 18:25:46 +0800

    new file:   cpp/sum_root_to_leaf_numbers.cpp

---
+++ cpp/sum_root_to_leaf_numbers.cpp
@@ -0,0 +1,18 @@
+#include "utils.h"
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x): val(x), left(NULL), right(NULL){}
+};
+
+
+class Solution{
+};
+
+int main(int argc, char **argv){
+  return 0;
+}
