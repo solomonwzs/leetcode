e2582f105178b54bab884031e3058a8b855de4ba
Solomon Ng <solomon.wzs@gmail.com>
2015-01-19 16:43:34 +0800

    new file:   cpp/pascals_triangle.cpp

---
+++ cpp/pascals_triangle.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include "utils.h"
+
+using namespace std;
+
+
+class Solution{
+ public:
+  vector<vector<int> > generate(int numRows){
+    vector<vector<int> > out;
+
+    for (int i=0; i<numRows; ++i){
+      vector<int> row(i+1);
+      row[0]=1;;
+      for (int j=1; j<i; ++j){
+        row[j]=out[i-1][j-1]+out[i-1][j];
+      }
+      row[i]=1;
+      out.push_back(row);
+    }
+
+    return out;
+  }
+};
+
+
+int main(int argc, char **argv){
+  Solution s;
+  vector<vector<int> > out=s.generate(atoi(argv[1]));
+
+  for (unsigned i=0; i<out.size(); ++i){
+    for (unsigned j=0; j<i+1; ++j){
+      printf("%d ", out[i][j]);
+    }
+    printf("\n");
+  }
+  return 0;
+}
