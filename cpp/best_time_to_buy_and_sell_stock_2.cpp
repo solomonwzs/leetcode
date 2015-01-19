499d5a0e9b12eab3781ddd41b1bc0d20674bcc8b
Solomon Ng <solomon.wzs@gmail.com>
2015-01-19 11:05:44 +0800

    new file:   cpp/best_time_to_buy_and_sell_stock_2.cpp

---
+++ cpp/best_time_to_buy_and_sell_stock_2.cpp
@@ -0,0 +1,36 @@
+#include <limits>
+#include <vector>
+#include "utils.h"
+
+using namespace std;
+
+
+class Solution{
+ public:
+  int maxProfit(vector<int> &prices){
+    if (prices.size()<2){
+      return 0;
+    }
+
+    int sum=0;
+    for (unsigned i=0; i<prices.size()-1; ++i){
+      if (prices[i+1]>prices[i]){
+        sum+=prices[i+1]-prices[i];
+      }
+    }
+    return sum;
+  }
+};
+
+
+int main(int argc, char **argv){
+  vector<int> prices;
+  for (int i=1; i<argc; ++i){
+    prices.push_back(atoi(argv[i]));
+  }
+
+  Solution s;
+  debug_log("%d\n", s.maxProfit(prices));
+
+  return 0;
+}
