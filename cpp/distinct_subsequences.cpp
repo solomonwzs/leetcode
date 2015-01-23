b854e4ebafea004a3e9867b9ea205f62f0d04fd6
Solomon Ng <solomon.wzs@gmail.com>
2015-01-23 10:50:16 +0800

    modified:   cpp/distinct_subsequences.cpp

--- cpp/distinct_subsequences.cpp
+++ cpp/distinct_subsequences.cpp
@@ -5,39 +5,63 @@
 using namespace std;
 
 
-struct char_count{
-  char ch;
-  int count;
-  char_count(char x, int y): ch(x), count(y){}
-};
-
-
 class Solution {
  public:
+  //int numDistinct(string S, string T){
+  //  unsigned slen=S.size();
+  //  unsigned tlen=T.size();
+
+  //  if (slen<tlen){
+  //    return 0;
+  //  } else if (tlen==0){
+  //    return 1;
+  //  }
+
+  //  vector<int> r1(slen);
+  //  r1[0]=S[0]==T[0]?1:0;
+  //  for (unsigned i=1; i<slen; ++i){
+  //    r1[i]=S[i]==T[0]?r1[i-1]+1:r1[i-1];
+  //  }
+
+  //  for (unsigned ti=1; ti<tlen; ++ti){
+  //    vector<int> r0=r1;
+
+  //    r1[ti-1]=0;
+  //    for (unsigned si=ti; si<slen; ++si){
+  //      r1[si]=S[si]==T[ti]?r0[si-1]+r1[si-1]:r1[si-1];
+  //    }
+  //  }
+
+  //  return r1[slen-1];
+  //}
+
   int numDistinct(string S, string T){
     unsigned slen=S.size();
     unsigned tlen=T.size();
 
-    vector<int> r0(slen);
-    vector<int> r1(slen);
+    if (slen<tlen){
+      return 0;
+    } else if (tlen==0){
+      return 1;
+    }
 
-    r0[0]=S[0]==T[0]?1:0;
+    vector<vector<int> > v(tlen);
+    v[0]=vector<int>(slen);
+    v[0][0]=S[0]==T[0]?1:0;
     for (unsigned i=1; i<slen; ++i){
-      r0[i]=S[i]==T[0]?r0[i]+1:r0[i];
+      v[0][i]=S[i]==T[0]?v[0][i-1]+1:v[0][i-1];
     }
 
     for (unsigned ti=1; ti<tlen; ++ti){
-      r1[ti]=(r0[ti-1]!=0 && S[ti]==T[ti])?1:0;
+      v[ti]=vector<int>(slen-ti);
+      v[ti][0]=(S[ti]==T[ti] && v[ti-1][0]==1)?1:0;
       for (unsigned si=ti+1; si<slen; ++si){
-        if (S[si]!=T[ti]){
-          r1[si]=r1[si-1];
-        } else{
-          r1[si]=r0[si];
-        }
+        int idx=si-ti;
+        v[ti][idx]=S[si]==T[ti]?v[ti-1][idx]+v[ti][idx-1]:v[ti][idx-1];
       }
     }
 
-    return 0;
+    return v[tlen-1][slen-tlen];
   }
 };
 
