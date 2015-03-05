11b531ee7627327da0ad201b2c305443be2e4a7b
Solomon Ng <solomon.wzs@gmail.com>
2015-03-05 11:35:47 +0800

    new file:   cpp/partition_list.cpp

---
+++ cpp/partition_list.cpp
@@ -0,0 +1,35 @@
+#include "utils.h"
+
+
+struct ListNode *partition(struct ListNode *head, int x){
+  struct ListNode *tail=NULL;
+  struct ListNode **greater_head=&tail;
+  struct ListNode **cur=&head;
+  while (*cur){
+    struct ListNode *p=*cur;
+    if (p->val>=x){
+      *cur=p->next;
+      p->next=NULL;
+      *greater_head=p;
+      greater_head=&p->next;
+    } else{
+      cur=&p->next;
+    }
+  }
+  *cur=tail;
+  return head;
+}
+
+
+int main(int argc, char **argv){
+  ListNode *head;
+  build_link_list_from_argv(head, argc-1, argv);
+  int x=atoi(argv[argc-1]);
+
+  partition(head, x);
+
+  print_link_list(head);
+  delete_link_list(head);
+
+  return 0;
+}
