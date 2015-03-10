ec13f6e9995f379c690c51f13cfdd04c1080ab4b
Solomon Ng <solomon.wzs@gmail.com>
2015-03-10 11:21:17 +0800

    modified:   cpp/insertion_sort_list.cpp 	new file:   cpp/remove_duplicates_from_sorted_list.cpp

--- cpp/insertion_sort_list.cpp
+++ cpp/insertion_sort_list.cpp
@@ -1,14 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "utils.h"
 
 using namespace std;
-
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x):val(x), next(NULL){}
-};
 
 
 class Solution {
---
+++ cpp/remove_duplicates_from_sorted_list.cpp
@@ -0,0 +1,40 @@
+#include "utils.h"
+
+
+struct ListNode *deleteDuplicates(struct ListNode *head){
+  struct ListNode *p=head;
+  while (p && p->next){
+    struct ListNode *next=p->next;
+    if (p->val==next->val){
+      p->next=next->next;
+      free(next);
+    } else{
+      p=p->next;
+    }
+  }
+
+  return head;
+}
+
+
+int main(int argc, char **argv){
+  ListNode *head;
+  ListNode **cur=&head;
+  for (int i=1; i<argc; ++i){
+    *cur=(ListNode *)malloc(sizeof(ListNode));
+    (*cur)->val=atoi(argv[i]);
+    cur=&((*cur)->next);
+  }
+
+  head=deleteDuplicates(head);
+  for (ListNode *i=head; i; i=i->next){
+    debug_log("%d\n", i->val);
+  }
+
+  while (head){
+    ListNode *p=head;
+    head=head->next;
+    free(p);
+  }
+  return 0;
+}
