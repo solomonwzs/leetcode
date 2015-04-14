b5fec06fa74c08e25527a2067da6c3d30153f650
Solomon Ng <solomon.wzs@gmail.com>
2015-04-14 10:24:20 +0800

    new file:   binary_tree_right_side_view.py 	new file:   leetcode.py

---
+++ python/binary_tree_right_side_view.py
@@ -0,0 +1,37 @@
+from leetcode import TreeNode
+
+
+class Solution:
+    def rightSideView(self, root):
+        if root==None:
+            return []
+
+        rsv=[]
+        curLevel=[root]
+        while len(curLevel)!=0:
+            rsv.append(curLevel[-1].val)
+            nextLevel=[]
+            for node in curLevel:
+                if node.left!=None:
+                    nextLevel.append(node.left)
+
+                if node.right!=None:
+                    nextLevel.append(node.right)
+
+            curLevel=nextLevel
+
+        return rsv
+
+
+
+if __name__=="__main__":
+    nodes=[]
+    for i in range(5):
+        nodes.append(TreeNode(i+1))
+    nodes[0].left=nodes[1]
+    nodes[0].right=nodes[2]
+    nodes[1].right=nodes[4]
+    nodes[2].right=nodes[3]
+
+    s=Solution()
+    print s.rightSideView(nodes[0])
---
+++ python/leetcode.py
@@ -0,0 +1,6 @@
+class TreeNode:
+    """ Definition for a binary tree node."""
+    def __init__(self, x):
+        self.val=x
+        self.left=None
+        self.right=None
