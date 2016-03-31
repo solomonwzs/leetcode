432dedc4fec125554735c527ba7a118bdb5fde08
solomon <solomon.wzs@gmail.com>
2016-03-31 22:17:15 +0800

    modified:   cpp/palindrome_partitioning_2.cpp

--- cpp/palindrome_partitioning_2.cpp
+++ cpp/palindrome_partitioning_2.cpp
@@ -8,6 +8,11 @@ using namespace std;
 class Solution {
  public:
   int minCut(string s){
+    return this->manacher(s);
+  }
+
+ private:
+  int minCut1(string s){
     int len=s.size();
     if (len==0){
       return 0;
@@ -23,7 +28,6 @@ class Solution {
     for (int i=0; i<len; ++i){
       for (unsigned j=0; j<v[i].size(); ++j){
         int next=v[i][j];
-        // string sub=s.substr(i, next-i+1);
         if (r[next+1]>r[i]+1){
           r[next+1]=r[i]+1;
         }
@@ -33,7 +37,66 @@ class Solution {
     return r[len];
   }
 
- private:
+  int manacher(string s){
+    string ns = "#";
+    int len=s.size();
+    for (int i=0; i<len; ++i){
+      ns.push_back(s[i]);
+      ns.push_back('#');
+    }
+    int nlen=ns.size();
+    vector<int> p(nlen);
+    vector<int> q(len);
+
+    for (int i=0; i<len; ++i){
+      q[i]=i;
+    }
+
+    p[0]=0;
+    int idx=0;
+    int mx=0;
+    for (int i=1; i<nlen; ++i){
+      if (i<mx){
+        p[i]=mx-i>=p[idx*2-i]?p[idx*2-i]:mx-i;
+      } else{
+        p[i]=0;
+      }
+
+      while (i-p[i]-1>=0 && i+p[i]+1<nlen &&
+             ns[i-p[i]-1]==ns[i+p[i]+1]){
+        ++p[i];
+      }
+
+      if (i+p[i]>mx){
+        idx=i;
+        mx=i+p[i];
+      }
+      
+      if (ns[i]!='#'){
+        if (i/2-1>0 && q[i/2]>q[i/2-1]+1){
+          q[i/2]=q[i/2-1]+1;
+        }
+      }
+      for (int j=ns[i]=='#'?i+1:i+2; j<=i+p[i]; j+=2){
+        int lidx=(i*2-j)/2;
+        int ridx=j/2;
+
+        if (lidx==0){
+          q[ridx]=0;
+        } else{
+          if (q[ridx]>q[ridx-1]+1){
+            q[ridx]=q[ridx-1]+1;
+          }
+          if (q[ridx]>q[lidx-1]+1){
+            q[ridx]=q[lidx-1]+1;
+          }
+        }
+      }
+    }
+
+    return q[len-1];
+  }
+
   vector< vector<int> > gen_jump_list(string s){
     int len=s.size();
 
