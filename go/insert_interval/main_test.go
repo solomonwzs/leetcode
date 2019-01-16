1549afd7cbaa05156dfb88e5d229cf0ccc902ca8
solomon <solomon.wzs@gmail.com>
2019-01-17 00:00:11 +0800

    modified:   main.go 	modified:   main_test.go

--- go/insert_interval/main_test.go
+++ go/insert_interval/main_test.go
@@ -4,12 +4,9 @@ import "testing"
 
 func TestInsert(t *testing.T) {
 	intervals := []Interval{
-		Interval{1, 2},
-		Interval{3, 5},
-		Interval{6, 7},
-		Interval{8, 10},
-		Interval{12, 16},
+		Interval{1, 3},
+		Interval{6, 9},
 	}
-	newInterval := Interval{17, 18}
+	newInterval := Interval{2, 5}
 	t.Log(insert(intervals, newInterval))
 }
