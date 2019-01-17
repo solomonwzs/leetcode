366fc9286c660349fb60192ddcd63692a66cb633
solomon <solomon.wzs@gmail.com>
2019-01-17 20:20:08 +0800

    new file:   .gitignore 	modified:   Makefile 	new file:   rust/insert_interval/Cargo.toml 	new file:   rust/insert_interval/src/main.rs

--- Makefile
+++ Makefile
@@ -7,32 +7,35 @@ CFLAGS 		= -Wall -fpic -g -c -std=gnu++11
 
 UTILS 		= cpp/utils.cpp
 
-LIB_PATH 	= $(shell pwd)/bin
+LIB_PATH 	= $(shell pwd)/build
 
 ALL_SOURCES	= $(wildcard ./cpp/*.cpp)
 SOURCES 	= $(filter-out $(UTILS), $(ALL_SOURCES))
 OBJECTS 	= $(SOURCES:%.cpp=%.o)
 
-VALGRIND 	= valgrind
+VALGRIND	= valgrind
 
 
 .cpp.o:
 	@echo -e "\033[01;33m[Compile]\033[0m $< ... "
 	@$(CC) $(CFLAGS) $< -o $@
 	@if [ "$<" != "$(UTILS)" ]; then \
-		$(CC) $@ -o $(patsubst cpp/%.cpp,bin/%,$<) -L$(LIB_PATH) \
+		$(CC) $@ -o $(patsubst cpp/%.cpp,build/%,$<) -L$(LIB_PATH) \
 			-l$(UTILS:cpp/%.cpp=%) -Wl,-R$(LIB_PATH); \
 	else \
 		$(CC) -shared -Wl,-soname,lib$(patsubst cpp/%.cpp,%,$<).so \
-			-o $(patsubst cpp/%.cpp,bin/lib%.so,$<) \
+			-o $(patsubst cpp/%.cpp,build/lib%.so,$<) \
 			$(patsubst %.cpp,%.o,$<); \
 	fi
 	@echo -e "\033[01;32m[OK]\033[0m $(patsubst cpp/%.cpp,%,$<)"
 
-all:$(UTILS:%.cpp=%.o) $(OBJECTS)
+all:pre $(UTILS:%.cpp=%.o) $(OBJECTS)
 
 clean:
-	@rm $(OBJECTS) ./bin/* 2>/dev/null
+	@rm $(OBJECTS) ./build/* 2>/dev/null
+
+pre:
+	@[ -d build ] || mkdir build
 
 mem_check: all
 	$(VALGRIND) \
@@ -41,4 +44,4 @@ mem_check: all
 		--show-reachable=yes \
 		--num-callers=20 \
 		--track-fds=yes \
-		./bin/implement_magic_dictionary
+		./build/implement_magic_dictionary
---
+++ rust/insert_interval/src/main.rs
@@ -0,0 +1,81 @@
+// Definition for an interval.
+#[derive(Debug, PartialEq, Eq)]
+pub struct Interval {
+    pub start: i32,
+    pub end: i32,
+}
+
+impl Interval {
+    #[inline]
+    pub fn new(start: i32, end: i32) -> Self {
+        Interval {
+            start,
+            end
+        }
+    }
+}
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn insert(intervals: Vec<Interval>, new_interval: Interval) -> Vec<Interval> {
+        let mut result:Vec<Interval> = Vec::new();
+
+        let mut i = 0;
+        while i < intervals.len() && intervals[i].end < new_interval.start {
+            let x = &intervals[i];
+            result.push(Interval::new(x.start, x.end));
+            i += 1;
+        }
+
+        let mut start = new_interval.start;
+        let mut end = new_interval.end;
+        while i < intervals.len() && intervals[i].start <= new_interval.end {
+            let x = &intervals[i];
+            if x.start < start {
+                start = x.start;
+            }
+            if x.end > end {
+                end = x.end;
+            }
+            i += 1;
+        }
+        result.push(Interval::new(start, end));
+
+        while i < intervals.len() {
+            let x = &intervals[i];
+            result.push(Interval::new(x.start, x.end));
+            i += 1;
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_work0() {
+        let intervals = vec![
+            Interval::new(1, 3),
+            Interval::new(6, 9)
+        ];
+        let new_interval = Interval::new(2, 5);
+        println!("{:?}", Solution::insert(intervals, new_interval));
+    }
+
+    #[test]
+    fn it_work1() {
+        let intervals = vec![
+            Interval::new(1, 2),
+            Interval::new(3, 5),
+            Interval::new(6, 7),
+            Interval::new(8, 10),
+            Interval::new(12, 16)
+        ];
+        let new_interval = Interval::new(4, 8);
+        println!("{:?}", Solution::insert(intervals, new_interval));
+    }
+}
