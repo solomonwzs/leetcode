893d8ead721fb27af1e219117602ba23340138ad
solomon <solomon.wzs@gmail.com>
2019-01-28 20:32:32 +0800

    modified:   main2.go 	modified:   main_test.go

--- go/odd_even_jump/main2.go
+++ go/odd_even_jump/main2.go
@@ -1,17 +1,20 @@
 package main
 
-import "sort"
+import (
+	"sort"
+)
 
 type orderList [][2]int
 
-func (l *orderList) add(elem [2]int) {
-	i := sort.Search(len(*l), func(i int) bool {
+func (l *orderList) add(elem [2]int) (i int) {
+	i = sort.Search(len(*l), func(i int) bool {
 		a := (*l)[i]
 		return elem[1] < a[1] || (elem[1] == a[1] && elem[0] < a[0])
 	})
 	*l = append(*l, [2]int{})
 	copy((*l)[i+1:], (*l)[i:])
 	(*l)[i] = elem
+	return
 }
 
 func oddEvenJumpsNew(a []int) int {
@@ -28,8 +31,26 @@ func oddEvenJumpsNew(a []int) int {
 	lower[n-1] = true
 	list.add([2]int{n - 1, a[n-1]})
 
+	res := 1
 	for i := n - 2; i >= 0; i-- {
+		idx := list.add([2]int{i, a[i]})
+
+		if hi := idx + 1; hi < len(list) {
+			higher[i] = lower[list[hi][0]]
+		}
+
+		if idx+1 < len(list) && list[idx+1][1] == a[i] {
+			lower[i] = higher[list[idx+1][0]]
+		} else if lo := idx - 1; lo >= 0 {
+			for ; lo-1 >= 0 && list[lo-1][1] == list[lo][1]; lo-- {
+			}
+			lower[i] = higher[list[lo][0]]
+		}
+
+		if higher[i] {
+			res += 1
+		}
 	}
 
-	return 0
+	return res
 }
--- go/odd_even_jump/main_test.go
+++ go/odd_even_jump/main_test.go
@@ -5,21 +5,25 @@ import "testing"
 func TestJump(t *testing.T) {
 	a := []int{10, 13, 12, 14, 15}
 	t.Log(oddEvenJumps(a))
+	t.Log(oddEvenJumpsNew(a))
 }
 
 func TestJump1(t *testing.T) {
 	a := []int{2, 3, 1, 1, 4}
 	t.Log(oddEvenJumps(a))
+	t.Log(oddEvenJumpsNew(a))
 }
 
 func TestJump2(t *testing.T) {
 	a := []int{5, 1, 3, 4, 2}
 	t.Log(oddEvenJumps(a))
+	t.Log(oddEvenJumpsNew(a))
 }
 
 func TestJump4(t *testing.T) {
 	a := []int{1, 2, 3, 2, 1, 4, 4, 5}
 	t.Log(oddEvenJumps(a))
+	t.Log(oddEvenJumpsNew(a))
 }
 
 func TestOrderList(t *testing.T) {
