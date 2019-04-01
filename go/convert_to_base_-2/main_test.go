b231e6c108f2514fc6a58bff17525c1cfe3b437f
solomon <solomon.wzs@gmail.com>
2019-04-01 11:16:30 +0800

    new file:   main.go 	new file:   main_test.go

---
+++ go/convert_to_base_-2/main_test.go
@@ -0,0 +1,14 @@
+/**
+ * @author  Solomon Ng <solomon.wzs@gmail.com>
+ * @version 1.0
+ * @date    2019-04-01
+ * @license MIT
+ */
+
+package main
+
+import "testing"
+
+func TestConv(t *testing.T) {
+	t.Log(baseNeg2(0))
+}
