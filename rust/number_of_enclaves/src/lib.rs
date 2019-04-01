afde7de58a88dbbf730038036a221c9ec085f273
solomon <solomon.wzs@gmail.com>
2019-04-01 20:33:39 +0800

    new file:   Cargo.toml 	new file:   src/lib.rs

---
+++ rust/number_of_enclaves/src/lib.rs
@@ -0,0 +1,15 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn num_enclaves(a: Vec<Vec<i32>>) -> i32 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
